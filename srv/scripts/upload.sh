d5a6b3f77b91f6429c83599d0b0358bebb28767e
Grant Haywood <grant@iowntheinter.net>
2016-02-04 09:18:31 -0700

    Update upload.sh

--- srv/scripts/upload.sh
+++ srv/scripts/upload.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo $1 >> /var/log/uploader.log
-echo scp -o StrictHostKeyChecking=no $1 grant@iowntheinter.net:~/newmotion/ 2>&1 >> /var/log/uploader.log
-scp -o StrictHostKeyChecking=no $1 grant@iowntheinter.net:~/newmotion/ 2>&1 >> /var/log/uploader.log
+DSTSVR='user@sshhost.org'
+echo scp -o StrictHostKeyChecking=no $1 $DSTSVR:~/newmotion/ 2>&1 >> /var/log/uploader.log
+scp -o StrictHostKeyChecking=no $1 $DSTSVR:~/newmotion/ 2>&1 >> /var/log/uploader.log
 PID=$$
 echo $$
 disown $$ 
